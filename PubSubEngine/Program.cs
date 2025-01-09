cbb73fd37df8fa35bc83d090fda61317ec46ff1c
SRDJAN <srdjandelic02@gmail.com>
2025-01-09 12:53:17 +0100

    add certs

--- PubSubEngine/Program.cs
+++ PubSubEngine/Program.cs
@@ -1,7 +1,11 @@
 ﻿using PubSubCore.Interfaces;
 using PubSubCore.Models;
+using PubSubInfrastructure;
 using PubSubServices;
 using PubSubServices.PubSubServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
 
 namespace PubSubEngine.ConsoleApp
 {
@@ -9,6 +13,58 @@ namespace PubSubEngine.ConsoleApp
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+                Console.WriteLine("Welcome {0}", srvCertCN);
+
+                NetTcpBinding binding = new NetTcpBinding(SecurityMode.Transport);
+                binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+                var publisherBaseAddress = new Uri("net.tcp://localhost:8080/Publisher");
+                ServiceHost publisherHost = new ServiceHost(typeof(PubSubEngineService));
+                publisherHost.AddServiceEndpoint(typeof(IPublisher), binding, publisherBaseAddress);
+
+                publisherHost.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+                publisherHost.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+                publisherHost.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+                publisherHost.Credentials.ServiceCertificate.Certificate = CertificateManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+                publisherHost.Open();
+                Console.WriteLine("Publisher service is running on net.tcp://localhost:8080/Publisher");
+
+
+                NetTcpBinding subscriberBinding = new NetTcpBinding(SecurityMode.Transport);
+                subscriberBinding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+                var subscriberService = new PubSubEngineService();
+                var subscriberBaseAddress = new Uri("net.tcp://localhost:8081/Subscriber");
+                var subscriberHost = new ServiceHost(subscriberService, subscriberBaseAddress);
+                subscriberHost.AddServiceEndpoint(typeof(ISubscriber), subscriberBinding, "");
+
+                subscriberHost.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+                subscriberHost.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+                subscriberHost.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+                subscriberHost.Credentials.ServiceCertificate.Certificate = CertificateManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+                subscriberHost.Open();
+                Console.WriteLine("Subscriber service is running on net.tcp://localhost:8081/Subscriber");
+
+
+
+                Console.WriteLine("Press Enter to stop the services...");
+                Console.ReadLine();
+
+                publisherHost.Close();
+                subscriberHost.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"Stack Trace: {ex.StackTrace}");
+            }
+
+
             // Kreiraj PubSubEngine
             IPubSubService pubSubService = new PubSubManager();
 
